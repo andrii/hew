6b962db4fdeb0786fa491d066f0e50fe41479bb9
Andrii Ponomarov <andrii.ponomarov@gmail.com>
2015-12-22 19:43:15 -0500

    Fix the CI build and the deprecation warning

--- features/crud.feature
+++ features/crud.feature
@@ -4,6 +4,6 @@ Feature: Drive scaffolded CRUD
     Given Hew, RSpec Rails and Capybara are installed
 
   Scenario: specs fail without a generated scaffold
-    Given I run `rails generate hew User email`
-    When I run `rspec`
+    Given I run `rails generate hew Post title`
+    When I run `bundle exec rspec`
     Then the examples should all fail
--- features/step_definitions/rspec_steps.rb
+++ features/step_definitions/rspec_steps.rb
@@ -2,6 +2,7 @@ Then /^the example(?:s)? should(?: all)? fail$/ do
   step %q{the output should not contain "0 examples"}
   step %q{the output should not contain "0 failures"}
   step %q{the exit status should be 1}
+  all_output = all_commands.map { |c| c.output }.join("\n")
   example_summary = /(\d+) examples?, (\d+) failures?/.match(all_output)
   example_count, failure_count = example_summary.captures
   expect(failure_count).to eq(example_count)
